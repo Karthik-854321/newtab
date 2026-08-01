ff8063e907e5768f966a2cf7fbd8a28d8fbfa884
Karthik-854321 <seelamdk968@gmail.com>
2026-08-01 15:49:16 +0530

    Initialize Vite project

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-import './style.css'
+import "./css/main.css";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello NewTab</h1>
-`
+import "./js/app";
+import "./js/clock";
+import "./js/greeting";
+import "./js/search";
+import "./js/weather";
+import "./js/todo";
+import "./js/notes";
+import "./js/quote";
+import "./js/pomodoro";
+import "./js/shortcuts";
+import "./js/theme";
